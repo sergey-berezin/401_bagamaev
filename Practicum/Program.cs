73fa1a008a2f184dc7301a413bd6e7d9cd97ed5d
Мурад Багамаев <true-barsuk@yandex.ru>
2021-10-07 16:15:16 +0300

    App v.1

---
+++ Practicum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RecognitionCoreLibrary;
+
+namespace Practicum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the full path of the directory:");
+            string dirPath = Console.ReadLine();
+            var recognitionResult = RecognitionCore.Recognise(dirPath);
+            foreach (var image in recognitionResult)
+            {
+                var results = image.Value;
+                foreach (var res in results)
+                {
+                    Console.WriteLine($"{image.Key}: {res.Label} found");
+                }
+            }
+        }
+    }
+}
